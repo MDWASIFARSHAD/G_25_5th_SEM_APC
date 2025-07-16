3521c9096d6abf42169f5838cbf5e2ffda4f2019
Md Wasif Arshad <4wasifock@gmail.com>
2025-07-16 22:55:56 +0530

    Annotation

---
+++ LEC11_16_JUL_Annotation/Annotation/Test1.java
@@ -0,0 +1,23 @@
+package Annotation_16_07;
+
+import java.util.ArrayList;
+
+public class Test1 extends Test{
+	
+	@Override
+	public void lastYearTaxFileDeduction()
+	{
+		
+	}
+	
+	@Deprecated
+	public void show()
+	{
+		
+	}
+	@SuppressWarnings("unchecked")
+	ArrayList ar = new ArrayList();
+	
+	
+
+}
