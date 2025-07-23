94e46ee5332cb008c11eb00c608ceac8c83d3598
Md Wasif Arshad <4wasifock@gmail.com>
2025-07-23 19:46:25 +0530

    MCQ Discussion

---
+++ LEC15_22_JUL_MCQ/MCQImplementation.java
@@ -0,0 +1,77 @@
+package MCQ_22_JUL;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.TreeSet;
+import java.util.Vector;
+import java.util.function.Function;
+
+public class MCQImplementation {
+	
+	public static void main(String[] args) throws IOException {
+		
+//		Function<Integer, Integer> multiplyByTwo = x -> x * 2;
+//		Function<Integer, Integer> addThree = x -> x + 3;
+//
+//		Function<Integer, Integer> combined = multiplyByTwo.andThen(addThree);
+//		System.out.println(combined.apply(5));
+		
+		
+//		FileWriter fw = new FileWriter("output.txt");
+//		fw.write("Java");
+//		fw.close();
+//
+//		FileReader fr = new FileReader("output.txt");
+//		System.out.print((char) fr.read());
+//		fr.close(); 
+		
+		
+//		try { int arr[] = new int[5]; arr[5] = 100; } 
+//		catch(ArrayIndexOutOfBoundsException e) 
+//		{ System.out.println("Error"); }
+		
+		
+//		Vector<Integer> v = new Vector<>(2);
+//		v.add(1);
+//		v.add(2);
+//		v.add(3);
+//		System.out.println(v.capacity());
+		
+		
+//		  try {
+//	            System.out.println("Inside try");
+//	            throw new RuntimeException("Error");
+//	        } finally {
+//	            System.out.println("Inside finally");
+//	        }
+		
+		
+		
+//		 Set<String> set = new TreeSet()<>(Collections.reverseOrder());
+//	        set.add("Zebra");
+//	        set.add("Apple");
+//	        set.add("Mango");
+//	        System.out.println(set.iterator().next());
+	        
+	        
+//	        try {
+//	            System.exit(0);
+//	        } finally {
+//	            System.out.println("Finally Block");
+//	        }
+	        
+	        
+	        
+//	        try {
+//	            int i = Integer.parseInt("abc");
+//	        } catch (NumberFormatException e) {
+//	            System.out.println("Error");
+//	        }
+
+
+
+
+	}
+
+}
