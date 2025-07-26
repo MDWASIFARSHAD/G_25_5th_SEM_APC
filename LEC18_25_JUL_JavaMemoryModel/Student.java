4fbcd37287cf71e0dbdf67b1e59ce10cc5edf60f
Md Wasif Arshad <4wasifock@gmail.com>
2025-07-26 16:46:14 +0530

    Java Memory Model (JVM Architecture)

---
+++ LEC18_25_JUL_JavaMemoryModel/Student.java
@@ -0,0 +1,24 @@
+package Java_memory_model_25_07;
+
+public class Student {
+	
+	int a;
+	String s;
+	int[] b;
+	double d;
+	
+	
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+	public void m2()
+	{
+		System.out.println("m2 method");
+	}
+	public void m3()
+	{
+		System.out.println("m2 method");
+	}
+
+}
