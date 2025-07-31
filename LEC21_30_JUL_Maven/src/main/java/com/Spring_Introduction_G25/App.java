2f148db0f74f0505ca1d82af31fee9b5a26dbf92
Md Wasif Arshad <4wasifock@gmail.com>
2025-07-31 10:35:18 +0530

    Spring core Implementation through MAVEN

---
+++ LEC21_30_JUL_Maven/src/main/java/com/Spring_Introduction_G25/App.java
@@ -0,0 +1,13 @@
+package com.Spring_Introduction_G25;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+    }
+}
---
+++ LEC21_30_JUL_Maven/src/main/java/com/Spring_Introduction_G25/BeanClass.java
@@ -0,0 +1,21 @@
+package com.Spring_Introduction_G25;
+
+public class BeanClass {
+	
+	private String name="wasif";
+	private int number=99844;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+}
